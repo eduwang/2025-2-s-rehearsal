09274a998917ed5b134b5bf28400e35dbda59913
eduwang <145199866+eduwang@users.noreply.github.com>
2025-08-21 14:59:48 +0900

    admin

--- src/indexMain.js
+++ src/indexMain.js
@@ -2,7 +2,7 @@
 import { auth, provider } from "./firebaseConfig";
 import { onAuthStateChanged, signInWithPopup, signOut } from "firebase/auth";
 
-const allowedAdmins = ["MhtH5gvH0RMv4yogqP4Tj6ki4Tp1", 'EWQ1oEDv8MTLq0xMy2pRpuP93vc2', "sCYx1gjxSucOHkqYAOqprosCCTt2"]; // 🔐 여기에 관리자 UID 넣기
+const allowedAdmins = ["P3GN9l3O2eaHNylv46FMA1JiKTD3", 'EWQ1oEDv8MTLq0xMy2pRpuP93vc2', "sCYx1gjxSucOHkqYAOqprosCCTt2"]; // 🔐 여기에 관리자 UID 넣기
 
 document.addEventListener("DOMContentLoaded", () => {
   const userInfo = document.getElementById("user-info");
